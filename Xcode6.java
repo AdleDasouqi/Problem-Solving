fb1228f0818d1278c7b021c90922612d7ada6258
Adledasouqi <adledasouqi12@outlook.com>
2022-03-26 19:49:26 +0300

    Problem-Solving

---
+++ Xcode6.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * Problem #6
+ * Java Program to Check Whether a Number is Even or Odd
+*/
+
+public class Xcode6{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.print("Enter a number: ");
+            int myNumber = input.nextInt();
+            input.close();
+            evenOdd(myNumber);
+        }catch(InputMismatchException e){
+            System.out.println("Please enter a number!");
+        }
+    }
+
+    public static void evenOdd(int number){
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(number + " is " + result);
+    }
+}
+
+
