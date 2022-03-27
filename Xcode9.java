4e6ae0890273e1596cd682c228b63c5bfa43401a
Adledasouqi <adledasouqi12@outlook.com>
2022-03-28 02:04:15 +0300

    Problem-Solving

---
+++ Xcode9.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * Problem #9
+ * Java Program to Check Whether a Number is Positive or Negative
+*/
+
+public class Xcode9 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.print("Enter a number: ");
+            int varNumber = input.nextInt();
+            input.close();
+            numbers(varNumber);
+        }catch(InputMismatchException e){
+            System.out.println("Please enter a number!");
+        }
+    }
+
+    public static void numbers(int number){
+        String result = (number > 0) ? " is a positive" : (number < 0) ? " is a negative" : (number == 0) ? " is neither positive nor negative" : " number is not found";
+        System.out.println(number + result);
+    }
+}
