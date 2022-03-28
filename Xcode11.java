ff2702ce6e280888911fd2fd4cb32058965106bf
Adledasouqi <adledasouqi12@outlook.com>
2022-03-28 22:58:54 +0300

    Problem-Solving

---
+++ Xcode11.java
@@ -0,0 +1,14 @@
+
+/*
+ * Problem #11
+ * Java Program to Display Alphabets (A to Z) using loop
+*/
+
+public class Xcode11{
+    public static void main(String[] args){
+        char letter;
+        for(letter = 'A'; letter <= 'Z'; letter++){
+           System.out.print(letter + " ");
+        }
+    }
+}
