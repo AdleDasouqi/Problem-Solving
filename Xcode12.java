43f928fde3ca642af51f55d18bfeb3bf4eaea2a5
Adledasouqi <adledasouqi12@outlook.com>
2022-03-31 23:27:13 +0300

    Problem-Solving

---
+++ Xcode12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/*
+ * Problem #12
+ * Java Program to Find Factorial of a Number 
+*/
+
+public class Xcode12 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a factorial: ");
+        int varNumber = input.nextInt();
+        input.close();
+        System.out.println("Factorial of " + varNumber + " = " + factorial(varNumber));
+    }
+
+    public static int factorial(int number){
+        int result = (number == 0) ? 1 : (number * factorial(number - 1));
+        return result; 
+    }
+}
